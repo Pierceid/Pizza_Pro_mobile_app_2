96c269a9f7540c62982d841de01c77e40ea6f607
Pierceid <128907409+Pierceid@users.noreply.github.com>
2024-03-14 14:10:14 +0100

    refactoring

--- app/src/main/java/com/example/pizza_pro_2/MainActivity.kt
+++ app/src/main/java/com/example/pizza_pro_2/MainActivity.kt
@@ -3,40 +3,9 @@ package com.example.pizza_pro_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.pizza_pro_2.navigation.nav_graph.NavGraph
-import com.example.pizza_pro_2.ui.theme.Ashen
-import com.example.pizza_pro_2.ui.theme.Black
-import com.example.pizza_pro_2.ui.theme.Grey
-import com.example.pizza_pro_2.ui.theme.Orange
 import com.example.pizza_pro_2.ui.theme.Pizza_Pro_2_Theme
-import com.example.pizza_pro_2.ui.theme.Red
-import com.example.pizza_pro_2.ui.theme.Sun
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/pizza_pro_2/database/MyViewModel.kt
+++ app/src/main/java/com/example/pizza_pro_2/database/MyViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.pizza_pro_2.database
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pizza_pro_2.database.entity.Order
